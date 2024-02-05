b6b46f212527548fa8e573bbc1b97a536f49c583
TedYJY <t.yap@digipen.edu>
2024-02-05 22:29:59 +0800

    Hopefully Stable

--- UXG1420_Unity/Assets/Scripts/Environment/Puzzle_3x3.cs
+++ UXG1420_Unity/Assets/Scripts/Environment/Puzzle_3x3.cs
@@ -5,12 +5,20 @@ using UnityEngine;
 public class Puzzle_3x3 : MonoBehaviour
 {
 
+    //Keeps track of surrounding pressure plates in 3x3
     public GameObject Top;
     public GameObject Down;
     public GameObject Left;
     public GameObject Right;
+
+    //Keeps track of the state of the pressure plate
     public bool IsActivated = false;
+
+    //Keeps track of when the player is able to press down the pressure plate
     public bool IsOnTop = false;
+
+    //Keeps track of how many plates are activated
+    public static int Tracker;
 
     // Start is called before the first frame update
     void Start()
@@ -21,31 +29,43 @@ public class Puzzle_3x3 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Checks for keydown, when player is colliding with trigger
         if (Input.GetKeyDown(KeyCode.F) && IsOnTop == true)
         {
+            //Activates the pressure plate
             Activation();
+
+            //Activates surrounding pressure plates
             ActivationSurrounding();
         }
     }
 
+    //Activation method
     private void Activation()
     {
+        //Checks if IsActivated is true, and changes the SpriteRenderer
         if (!IsActivated)
         {
             this.gameObject.GetComponent<SpriteRenderer>().color = Color.red;
             IsActivated = true;
+            Tracker++;
         }
 
+        //Checks if IsActivated is false, and changes the SpriteRenderer
         else if (IsActivated)
         {
             this.gameObject.GetComponent<SpriteRenderer>().color = Color.white;
             IsActivated = false;
+            Tracker--;
         }
 
     }
 
+    //Activate surrounding pressure plates method
     private void ActivationSurrounding()
     {
+
+        //Checks if the GameObjects are pointing towards anything, to prevent errors
         if (Top != null)
         {
             Top.GetComponent<Puzzle_3x3>().Activation();
@@ -67,11 +87,14 @@ public class Puzzle_3x3 : MonoBehaviour
         }
     }
 
+
+    //Enters trigger, sets IsOnTop to true to allow Keydown
     private void OnTriggerEnter2D(Collider2D col)
     {
         IsOnTop = true;
     }
 
+    //Exits trigger, sets IsOnTop to false to prevent Keydown
     private void OnTriggerExit2D(Collider2D col)
     {
         IsOnTop = false;
