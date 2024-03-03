0dcdc13bb5b3df3c6c70c73b30918c2cfb1da749
dolphrun <angjoel1159@gmail.com>
2024-03-03 19:50:28 +0800

    Added a simple dialogue manager
    -need to hide it during screen transition
    -need to set trigger to something else
    -need to disappear after finishing dialogue

---
+++ UXG1420_Unity/Assets/Scripts/UI/DialogueTriggers.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTriggers : MonoBehaviour
+{
+    public Dialogue dialogue;
+
+
+    public void TriggerDialogue()
+    {
+        FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+    }
+
+}
