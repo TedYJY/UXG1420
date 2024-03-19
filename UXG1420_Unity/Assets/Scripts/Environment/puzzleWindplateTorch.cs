728d4b7489b1fed0304196ec7bbee11e535a04a3
zekingfx <zekingfx@gmail.com>
2024-03-19 17:41:41 +0800

    Working to make animations fade
    Updated some water

--- UXG1420_Unity/Assets/Scripts/Environment/puzzleWindplateTorch.cs
+++ UXG1420_Unity/Assets/Scripts/Environment/puzzleWindplateTorch.cs
@@ -20,7 +20,6 @@ public class puzzleWindplateTorch : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        TorchCollider = GameObject.Find("Torch_Collider");
         IsAble = false;
     }
 
